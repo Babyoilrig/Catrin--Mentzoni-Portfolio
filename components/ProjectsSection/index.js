65f7e9ebd61d1a6173ff2a6c61ae2388de0ad53a
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-03-16 17:25:04 +0000

    ProjectSection3 working

--- components/ProjectsSection/index.js
+++ components/ProjectsSection/index.js
@@ -4,16 +4,17 @@ import css from './ProjectSection.module.css'
 
 export default function ProjectsSection1() {
   return (
+    //   Grid for project section 1
     <div class="grid gap-4 grid-cols-1 md:grid-cols-6 grid-rows-1 place-items-center content-center border-solid border-2 border-black rounded-lg mb-12 mt-8">
     
       {/* Text box for project 1 */}
       <div class="col-span-3 content-center mx-40" className={css.proj1Left}>
       <div className={css.projectTextBox}>
         <h2 className={css.projectTitle}>This Website</h2>
-        <h3 className={css.projectDescription}>"This portfolio site was made with Next.js and tailwindcss. My aim was 
+        <h3 className={css.projectDescription}>This portfolio site was made with Next.js and tailwindcss. My aim was 
             to create a stylish and feminine personal site, which is easy 
             to use and responsive on mobile devices. It was a steep learning curve - in which I spent hours upon hours learning 
-            about mobile-responsive containers and wrapping divs, but I am proud of the result :)"</h3>
+            about mobile-responsive containers and wrapping divs, but I am proud of the result :)</h3>
         <Link
           href="https://github.com/Babyoilrig/CatrinMentzoni-Portfolio"
           passHref
@@ -24,7 +25,7 @@ export default function ProjectsSection1() {
       </div>
       {/* Image box for project1 */}
       <div class="col-span-3 place-items-center" >
-      <div class=""className={css.projectImageBox}>
+      <div className={css.projectImageBox}>
         <img
           className={css.bootcamperAppImage}
           src="/images/mywebsite-sml.png"
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Footer from '../components/Footer'
 import ProjectsSection1 from '../components/ProjectsSection';
+import ProjectsSection3 from '../components/ProjectSection3';
 
 
 
@@ -8,6 +9,7 @@ export default function Home() {
     <div>
       <h1>Hello World!</h1>
       <ProjectsSection1 />
+      <ProjectsSection3 />
       <Footer />
     </div>
   );
