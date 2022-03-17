84065a6a05c83d1054a825b906e00a2f40e332eb
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-03-17 13:28:55 +0000

    My website image updated

---
+++ components/BalloonGirlSection/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function BalloonImage() {
+  return (
+    <div>
+        <img
+          className={css.balloonGirlImage}
+          src="/images/girl-with-balloon-no-background.png"
+        />
+    </div>
+  )
+}
