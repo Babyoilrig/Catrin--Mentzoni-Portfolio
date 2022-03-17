43d5450c58d207ebc60798cfcd4c9f58b164b6ae
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-03-17 11:58:29 +0000

    Changed size of fonts on navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -15,7 +15,7 @@ const [active, setActive] = useState(false);
       <nav className="flex items-center flex-wrap bg-white-400 p-3 ">
         <Link href="/">
           <a className="inline-flex items-center p-2 mr-4 ">
-            <span className="text-xl text-black font-bold uppercase tracking-wide">
+            <span className="text-3xl text-black font-bold uppercase tracking-wide">
               CV
             </span>
           </a>
@@ -46,17 +46,17 @@ const [active, setActive] = useState(false);
         >
           <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
             <Link href="/">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-white-600 hover:text-black ">
+              <a className="2xl:inline-flex text-2xl lg:w-auto w-full px-5 py-3 rounded text-black font-bold items-center justify-center hover:bg-white-600 hover:text-black ">
                 Home
               </a>
             </Link>
             <Link href="/about">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-white-600 hover:text-black">
+              <a className="2xl:inline-flex text-2xl lg:w-auto w-full px-5 py-3 rounded text-black font-bold items-center justify-center hover:bg-white-600 hover:text-black">
                 About
               </a>
             </Link>
             <Link href="#projects">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-white-600 hover:text-black">
+              <a className="2xl:inline-flex text-2xl lg:w-auto w-full px-5 py-3 rounded text-black font-bold items-center justify-center hover:bg-white-600 hover:text-black">
                 Projects
               </a>
             </Link>
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ import IntroPage1 from '../components/Intro';
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
       <IntroPage1 />
       <YoutubeVideo />
       <ProjectHeading />
