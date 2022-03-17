1f498ebdbfe9cd1db0e9195f84e7392551d8ad8c
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-03-17 11:17:01 +0000

    BodyText section done

---
+++ components/BodyText/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function BodyText() {
+  return (
+    <div class="grid gap-0 grid-cols-1 grid-rows-1 place-items-center content-center mt-8">
+    <p class="text-left font-roboto text-xl">On returning to the UK in 2012, I trained to be a primary school teacher, a job which I did for 
+10 years. Although I enjoyed the job, the lack of opportunities to learn new skills, and the lack of challenge motivated me to begin looking for a new career path. During spring 2021 I read some articles regarding the skills shortage in the software
+ engineering sector which mentioned the School of Code and they’re amazing bootcamp. During the summer I decided to apply for the course. Although the bootcamp was extremely challenging, I soon regained my love of coding!</p>
+
+<p class="text-left font-roboto text-xl"></p>
+
+<p class="text-left font-roboto text-xl"></p>
+    </div>
+  )
+}
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import Footer from '../components/Footer'
 import DeveloperSection from '../components/DeveloperSection'
 import HobbiesSection from '../components/Hobbies';
+import BodyText from '../components/BodyText';
 
 export default function About() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <BodyText />
         <DeveloperSection />
         <HobbiesSection />
       <Footer />
