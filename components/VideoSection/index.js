617efff84590ddf0f84fd2f94e452884a9f33c20
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-03-23 16:38:01 +0000

    Adding colour to divs and padding to see if I can identify why video not flexing with Tailwind styling

--- components/VideoSection/index.js
+++ components/VideoSection/index.js
@@ -17,7 +17,7 @@ extends React.Component {
       <div class="grid gap-0 grid-cols-1 grid-rows-1 place-items-center content-center border-solid border-2 border-black rounded-lg mt-8 w-full md:w-screen ">
       <h1 class="text-center font-roboto text-5xl font-bold">Introduction</h1>
       <br/><br/>
-      <div class="grid gap-0 grid-cols-1 grid-rows-1 place-items-center content-center w-screen md:w-auto">
+      <div className= {css.videoContainer} class="grid gap-0 grid-cols-1 grid-rows-1 place-items-center content-center w-[100px] md:w-full">
         <YouTube className={css.video} videoId="GNbKZls3W4w" 
             opts={opts} onReady={this._onReady} />
             </div>
