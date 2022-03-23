f4bb5c41f49620e885ee97b1bd3ee4a6ec684783
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-03-23 16:24:01 +0000

    Commented out default width and height and put in mobile responsive div to try

--- components/VideoSection/index.js
+++ components/VideoSection/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import YouTube from "react-youtube";
+import css from './VideoSection.module.css';
 
 export default class YoutubeVideo 
 extends React.Component {
   render() {
     const opts = {
-      height: "390",
-      width: "640",
+      // height: "390",
+      // width: "640",
       playerVars: {
         autoplay: 1,
       },
@@ -16,8 +17,8 @@ extends React.Component {
       <div class="grid gap-0 grid-cols-1 grid-rows-1 place-items-center content-center border-solid border-2 border-black rounded-lg mt-8 w-full md:w-screen ">
       <h1 class="text-center font-roboto text-5xl font-bold">Introduction</h1>
       <br/><br/>
-      <div id="video-container">
-        <YouTube id="video" videoId="GNbKZls3W4w" 
+      <div class="grid gap-0 grid-cols-1 grid-rows-1 place-items-center content-center w-screen md:w-auto">
+        <YouTube className={css.video} videoId="GNbKZls3W4w" 
             opts={opts} onReady={this._onReady} />
             </div>
             <br/><br/><br/>
