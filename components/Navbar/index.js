b89f94ee6c2d89973126969495fcb9af8158a2e9
babyoilrig <catrin.mentzoni@yahoo.com>
2023-09-25 20:06:04 +0100

    Uploaded new CV

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -13,7 +13,7 @@ const [active, setActive] = useState(false);
   return (
     <>
       <nav className="flex items-center flex-wrap bg-white-400 p-3 ">
-        <Link href="https://drive.google.com/file/d/13l5EDVpTSWMwYZp8rLr1gorKFqCm6Tba/view?usp=sharing">
+        <Link href="https://drive.google.com/file/d/1-BUWk9mtWsMWbPC9w7BdS8-mGFS3FhFC/view?usp=sharing">
           <a className="inline-flex items-center p-2 mr-4 ">
             <span className="text-3xl text-black font-bold uppercase tracking-wide">
               CV
