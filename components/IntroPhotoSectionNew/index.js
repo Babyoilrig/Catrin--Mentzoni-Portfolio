51d34b4e6d5b161319234d482eb8c235b171280f
babyoilrig <catrin.mentzoni@yahoo.com>
2024-09-17 14:17:13 +0100

    Styling new divs

--- components/IntroPhotoSectionNew/index.js
+++ components/IntroPhotoSectionNew/index.js
@@ -6,26 +6,29 @@ export default function IntroPhotoSectionNew() {
   return (
     <div class="grid gap-4 grid-cols-1 grid-rows-1 place-items-center content-center mb-12 mt-8 w-screen">
     {/* Main container */}
-    <div className={css.introPhotoSection}>
+    <div className={css.mainIntroContainer}>
+    <div className={css.introTextSection}>
     {/* //Left Section - Heading */}
-    <div className={css.introPhotoSectionTop}>
+    <div className={css.introTextSectionTop}>
       <h1 className={css.fullstackHeading}>Fullstack Software Engineer</h1>
       <h2 className={css.nameHeading}>Hello, my name is</h2>
       <h2 className={css.nameHeading}>Catrin Mentzoni</h2>
       </div>
       {/* //Right Section */}
-      <div className={css.introPhotoSectionBottom}>
+      <div className={css.introTextSectionBottom}>
       <div className={css.textSection}>
       <p>I am an enthusiastic and friendly individual with excellent communication and listening skills, who thrives when working as part of a team. I love learning new things, and I also love a challenge! I previously worked as a primary school teacher for ten years, but learned to code as part of the School of Code's Bootcamp 10 cohort - and I love it!</p>
       </div>
       </div>
       
-    </div>
-    
-
+      </div>
       <div className={css.imageSection}>
       <img className={css.catImage} src="/Images/cat-big.png"></img>
       </div>
+    </div>
+    
+
+      
 
         <div class="grid gap-4 grid-cols-2 grid-rows-1 place-items-center content-center mb-12 mt-8">
           <Link href="/about" passHref>
