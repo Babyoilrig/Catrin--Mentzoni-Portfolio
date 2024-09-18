0e453b66766d75e76e0e1d679c601ad68bd8645f
babyoilrig <catrin.mentzoni@yahoo.com>
2024-09-18 14:21:06 +0100

    Adding general styling to other project sections

--- components/ProjectSection3/index.js
+++ components/ProjectSection3/index.js
@@ -5,10 +5,13 @@ import css from './ProjectSection3.module.css'
 export default function ProjectsSection3() {
   return (
     //   Grid for project section 3
-    <div class="grid gap-0 grid-cols-1 md:grid-cols-6 grid-rows-1 place-items-center content-center border-solid border-2 border-black rounded-lg mb-12 mt-8 ml-3 mr-3 pl-6 pr-6 pt-5 pb-5">
+    // <div class="grid gap-0 grid-cols-1 md:grid-cols-6 grid-rows-1 place-items-center content-center border-solid border-2 border-black rounded-lg mb-12 mt-8 ml-3 mr-3 pl-6 pr-6 pt-5 pb-5">
+    <div className={css.projectSection1}>
+
     
       {/* Text box for project 3 */}
-      <div class="col-span-3 content-center place-items-center md:mx-40" className={css.proj1Left}>
+      {/* <div class="col-span-3 content-center place-items-center md:mx-40" className={css.proj1Left}> */}
+      <div className={css.proj1Left}>
       <div className={css.projectTextBox}>
         <h2 className={css.projectTitle}>Tribe App</h2>
         <h3 className={css.projectDescription}>I worked as part of a team of 6 to create the Tribe mobile app, as part of our month long final project at the School of Code. This app assists with family organisation, and allows parents to assign chores to their children and other family members. We are building the project using React Native and Firebase. So far, as well as learning a new flavour of React, I have researched and implemented a testing environment and written tests, created a stacknavigator component and researched how to deploy and host apps via the Expo app store, amongst many other things.</h3>
@@ -21,7 +24,8 @@ export default function ProjectsSection3() {
       </div>
       </div>
       {/* Image box for project3 */}
-      <div class="col-span-3 content-center place-items-center md:mx-40" >
+      {/* <div class="col-span-3 content-center place-items-center md:mx-40" > */}
+      <div>
       <div className={css.projectImageBox}>
         <img
           className={css.bootcamperAppImage}
--- components/ProjectSection4/index.js
+++ components/ProjectSection4/index.js
@@ -5,11 +5,13 @@ import css from './ProjectSection4.module.css'
 export default function ProjectsSection4() {
   return (
     //   Grid for project section 4
-    <div class="grid gap-0 grid-cols-1 md:grid-cols-6 grid-rows-1 place-items-center content-center border-solid border-2 border-black rounded-lg mb-12 mt-8 ml-3 mr-3 pl-6 pr-6 pt-5 pb-5">
+    // <div class="grid gap-0 grid-cols-1 md:grid-cols-6 grid-rows-1 place-items-center content-center border-solid border-2 border-black rounded-lg mb-12 mt-8 ml-3 mr-3 pl-6 pr-6 pt-5 pb-5">
+    <div className={css.projectSection1}>
     
       
       {/* Image box for project4 */}
-      <div class="col-span-3 content-center place-items-center md:mx-40" >
+      {/* <div class="col-span-3 content-center place-items-center md:mx-40" > */}
+      <div className={css.proj1Left}>
       <div className={css.projectImageBox}>
         <img
           className={css.musicSalesAppImage}
@@ -19,7 +21,8 @@ export default function ProjectsSection4() {
       </div>
     
     {/* Text box for project 4 */}
-    <div class="col-span-3 content-center place-items-center md:mx-40" className={css.proj1Left}>
+    {/* <div class="col-span-3 content-center place-items-center md:mx-40" className={css.proj1Left}> */}
+    <div>
     <div className={css.projectTextBox}>
       <h2 className={css.projectTitle}>Music Sales App</h2>
       <h3 className={css.projectDescription}>This is an app I built during an internship with the company Vista Health between January - June 2024. The app was developed for a large blue chip pharmaceutical company to display their global sales information. I built the frontend of this app completely independently using VanillaJS and Chart.js charting library. This application was designed to be viewed on desktop – due to the complex graphical data it displays.</h3>
