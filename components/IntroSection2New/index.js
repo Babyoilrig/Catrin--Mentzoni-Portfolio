6a3367d643aebf8f07cda42d67b9272d3c3a0f17
babyoilrig <catrin.mentzoni@yahoo.com>
2024-09-19 12:49:41 +0100

    Adding divs and beginning to style the about me intro section

---
+++ components/IntroSection2New/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+import css from './IntroSection2New.module.css'
+
+export default function IntroSection2New() {
+  return (
+    <div>
+        {/* <div class="grid gap-4 grid-cols-1 grid-rows-1 place-items-center content-center mb-12 mt-8 w-screen"> */}
+        {/* <div className={css.introContainer}> */}
+        <div>
+        <div className={css.introContainer}>
+        <div className={css.introLeft}>
+        <h1 className={css.heading}>About me</h1>
+        </div>
+        <div className={css.introRight}><img src="/images/cat-big.png"/></div>
+    
+    </div>
+    </div>
+    </div>
+  )
+}
--- pages/about.js
+++ pages/about.js
@@ -3,11 +3,13 @@ import DeveloperSection from '../components/DeveloperSection'
 import HobbiesSection from '../components/Hobbies';
 import BodyText from '../components/BodyText';
 import IntroSection2 from '../components/IntroPage2';
+import IntroSection2New from '../components/IntroSection2New';
 
 export default function About() {
   return (
     <div>
       <IntroSection2 />
+      {/* <IntroSection2New /> */}
       <BodyText />
         <DeveloperSection />
         <HobbiesSection />
