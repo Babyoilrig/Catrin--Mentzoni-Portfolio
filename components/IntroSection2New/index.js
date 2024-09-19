2a16df7019f76b104225652bc9f897db2cc4f3d8
babyoilrig <catrin.mentzoni@yahoo.com>
2024-09-19 13:27:40 +0100

    Adding styling to intro section on about page

--- components/IntroSection2New/index.js
+++ components/IntroSection2New/index.js
@@ -8,14 +8,19 @@ export default function IntroSection2New() {
         {/* <div class="grid gap-4 grid-cols-1 grid-rows-1 place-items-center content-center mb-12 mt-8 w-screen"> */}
         {/* <div className={css.introContainer}> */}
         <div>
-        <div className={css.introContainer}>
-        <div className={css.introLeft}>
-        <h1 className={css.heading}>About me</h1>
+            <div className={css.introContainer}>
+                <div className={css.introLeft}>
+                    {/* <div className={css.headingContainer}> */}
+                    <h1 className={css.heading}>About me</h1>
+                    {/* </div> */}
+            
+                </div>
+                        <div className={css.introRight}>
+                            <img className= {css.catPic}src="/images/cat-big.png"/>
+                        </div>  
+                
+            </div>
         </div>
-        <div className={css.introRight}><img src="/images/cat-big.png"/></div>
-    
-    </div>
-    </div>
     </div>
   )
 }
